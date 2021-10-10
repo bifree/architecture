beeb1fe22ab173e97aaca80e9d130feecc257cec
bifree <bifree2bfree@gmail.com>
2021-10-10 23:59:47 +0900

    スライド

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -55,3 +55,37 @@ jQuery(function() {
 	}
 });
 });
+
+$(function(){
+
+	var slider = "#slider"; // スライダー
+	var thumbnailItem = "#thumbnail-list .thumbnail-item"; // サムネイル画像アイテム
+	
+	// サムネイル画像アイテムに data-index でindex番号を付与
+	$(thumbnailItem).each(function(){
+	var index = $(thumbnailItem).index(this);
+	$(this).attr("data-index",index);
+	});
+	
+	// スライダー初期化後、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
+	$(slider).on('init',function(slick){
+	var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
+	$(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
+	});
+
+	//slickスライダー実行
+	$(slider).slick();
+	//サムネイル画像アイテムをクリックしたときにスライダー切り替え
+	$(thumbnailItem).on('click',function(){
+			var index = $(this).attr("data-index");
+			$(slider).slick("slickGoTo",index,false);
+	});
+	
+	//サムネイル画像のカレントを切り替え
+	$(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
+			$(thumbnailItem).each(function(){
+			$(this).removeClass("thumbnail-current");
+			});
+			$(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
+	});
+});
---
+++ WordPressTheme/page-1010.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <title></title>
+    <meta name="description" content="">
+    <!-- font-family -->
+    
+    <!-- slick.css -->
+    <link rel="stylesheet" href="./css/slick.css">
+    <link rel="stylesheet" href="./css/slick-theme.css">
+    <!-- main.css -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <main class="main">
+        <section class="custom-home inner">
+            <h2 class="custom-home-title">注文住宅</h2>
+            <div class="custom-home-arrow-box">
+                <div class="custom-home-arrow"></div>
+            </div>
+            <div class="custom-home-slider-area">
+                <div class="custom-home-slider-area__bg">
+                    <h3 class="custom-home-slider-area__title">常滑市　K様邸</h3>
+                    <ul id="slider" class="custom-home-slider-area__img-wrap">
+                        <li class="custom-home-slider-area__img slider-01 vertical-img">
+                            <img src="./image/01.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-02 horizontal-img">
+                            <img src="./image/02.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-03 horizontal-img">
+                            <img src="./image/03.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-04 horizontal-img">
+                            <img src="./image/04.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-05 horizontal-img">
+                            <img src="./image/05.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-06 horizontal-img">
+                            <img src="./image/06.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-07 horizontal-img">
+                            <img src="./image/07.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-08 horizontal-img">
+                            <img src="./image/08.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-09 vertical-img">
+                            <img src="./image/09.png" alt="">
+                        </li>
+                        <li class="custom-home-slider-area__img slider-10 horizontal-img">
+                            <img src="./image/10.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+                <div class="custom-home-slider__thumb-area">
+                    <ul id="thumbnail-list" class="custom-home-slider__thumb-area-wrap">
+                        <li class="thumbnail-item slider-01 vertical-img thumbnail-current">
+                            <img src="./image/01.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-02 horizontal-img">
+                            <img src="./image/02.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-03 horizontal-img">
+                            <img src="./image/03.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-04 horizontal-img">
+                            <img src="./image/04.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-05 horizontal-img">
+                            <img src="./image/05.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-06 horizontal-img">
+                            <img src="./image/06.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-07 horizontal-img">
+                            <img src="./image/07.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-08 horizontal-img">
+                            <img src="./image/08.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-09 vertical-img">
+                            <img src="./image/09.png" alt="">
+                        </li>
+                        <li class="thumbnail-item slider-10 horizontal-img">
+                            <img src="./image/10.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+
+<!-- script -->
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- script.js -->
+    <script src="./js/script.js"></script>
+    <!-- script.js -->
+    <script src="./js/slick.js"></script>
+<!-- script end -->
+</body>
+</html>
