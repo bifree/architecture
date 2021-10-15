10bf3831a462741b070b616d7f4ab371a02dd735
bifree <bifree2bfree@gmail.com>
2021-10-15 21:02:29 +0900

    スライダー一応OK？

--- WordPressTheme/footer.php
+++ WordPressTheme/footer.php
@@ -2,7 +2,7 @@
 	<div class="l-footer">
 		<div class="footer">
 			<div class="footer__info">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-sm.png" alt="">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-sm.png" alt=""></a>
 				<p>リフォーム、注文住宅は碧南市の大友建築</p>
 				<p>〒447-0013     愛知県碧南市白沢町</p>
 				<p>TEL	0566-42-8648</p>
