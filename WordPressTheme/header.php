a1e18b39b01b090fff0d25ef9e2432f6397a743a
bifree <bifree2bfree@gmail.com>
2021-10-15 18:36:01 +0900

    フォント変更

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -11,16 +11,9 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/slick-theme.css">
   <!-- main.css -->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style1.css">
-  <script>
-  (function(d) {
-    var config = {
-      kitId: 'ojk1wjf',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Noto+Serif+JP:wght@300;400&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 
